80d44298c6e4772432ad100bbcfc0a960fd97ffa
Tyler Moon <moon.tyler@gmail.com>
2017-12-30 16:26:59 -0500

    Formatting changes

--- simple_image.py
+++ simple_image.py
@@ -1,12 +1,15 @@
 # Using the squeezenet library https://github.com/rcmalli/keras-squeezenet/
 
 import numpy as np
+# Keras
 from keras_squeezenet import SqueezeNet
 from keras.applications.imagenet_utils import preprocess_input, decode_predictions
 from keras.preprocessing import image
+# Utilties
 import sh
 import os
 import sys
+# PythonImageLibrary
 from PIL import Image
 from PIL import ImageFont
 from PIL import ImageDraw
@@ -20,8 +23,8 @@ x = image.img_to_array(img)
 x = np.expand_dims(x, axis=0)
 x = preprocess_input(x)
 
-preds = decode_predictions(model.predict(x))[0]
-most_likely = preds[0]
+preds = decode_predictions(model.predict(x))
+most_likely = preds[0][0]
 most_likely_object = most_likely[1]
 most_likely_percent = most_likely[2]
 
@@ -41,5 +44,7 @@ out = Image.alpha_composite(base,txt)
 out.show()
 
 print '+++++++++++++++++++++++++++++++++++++++++++\n\n'
-print preds
+for results in preds:
+    for result in results:
+        print('Probability %0.2f%% => [%s]' % (100*result[2], result[1]))
 print '\n\n+++++++++++++++++++++++++++++++++++++++++++'
